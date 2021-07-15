09e3a46e02b04d1f6711a513ef41ccd3fadba028
tsergiiv <tsergiiv@gmail.com>
2021-07-15 09:21:54 +0300

    add middle class, move faq section to footer

--- wp-content/themes/exchange/single-exchange.php
+++ wp-content/themes/exchange/single-exchange.php
@@ -8,7 +8,7 @@ Template Post Type: exchange
 <?php get_header(); ?>
 
     <main class="content">
-        <div class="article">
+        <div class="article middle">
             <section class="article-head container">
                 <div class="breadcrumbs">
                     <a class="breadcrumbs-link" href="<?= home_url() ?>">Main Page</a>
@@ -419,48 +419,6 @@ Template Post Type: exchange
                 <div class="article-sidebar">
                     <div class="article-sidebar-title">Table of contents</div><a class="article-sidebar-link active" href="">What is <?= the_title() ?>?</a><a class="article-sidebar-link" href="">Pros/Cons</a><a class="article-sidebar-link" href="">Fees and Trading</a><a class="article-sidebar-link" href="">Exchange Info</a><a class="article-sidebar-link" href="">Security</a><a class="article-sidebar-link" href="">Tips for users</a><a class="article-sidebar-link" href="">Regulation</a><a class="article-sidebar-link" href="">Privacy</a><a class="article-sidebar-link" href="">User Experience</a><a class="article-sidebar-link" href="">Interface </a><a class="article-sidebar-link" href="">How does it compare</a><a class="article-sidebar-link" href="">Customer Support</a><a class="article-sidebar-link" href="">Open Account and Trade</a><a class="article-sidebar-link" href="">Fees</a><a class="article-sidebar-link" href="">Services</a>
                 </div>
-            </section>
-            <section class="faq">
-                <div class="faq-head">
-                    <div class="section-title-simple">FAQ</div>
-                    <h1 class="section-header m"><span class="word-accent violet">New to crypto? Here’s our guide!<span class="word-accent-square left"></span><span class="word-accent-square right"></span></span></h1>
-                </div>
-                <div class="faq-wrap"><a class="faq-elem" href="">
-                        <div class="faq-elem-content"><img class="faq-elem-img" src="<?php bloginfo('template_url'); ?>/assets/img/content/faq/faq-img-1.png" alt="FAQ Image #1"><span class="faq-elem-number">01</span><span class="faq-elem-title">
-                 Why Bitcoin? </span><span class="faq-elem-text">
-                It seems like the buzz around Bitcoin
-                just keeps growing. Bitcoin is a constantly
-                evolving technology with a passionate
-                community. What is it about the digital
-                asset that makes it such a controversial
-                topic and attractive investment? </span></div></a><a class="faq-elem" href="">
-                        <div class="faq-elem-content"><img class="faq-elem-img" src="<?php bloginfo('template_url'); ?>/assets/img/content/faq/faq-img-2.png" alt="FAQ Image #2"><span class="faq-elem-number">02</span><span class="faq-elem-title">
-                 How to Get Started with Bitcoin</span><span class="faq-elem-text">
-                It can be difficult to get past the
-                technical jargon surrounding Bitcoin in
-                order to figure out how you can buy and
-                store the digital asset. Beginners can
-                purchase BTC on many exchanges with fiat
-                currency. Once you’ve purchased crypto,
-                invest in a safe  offline storage solution. </span></div></a><a class="faq-elem" href="">
-                        <div class="faq-elem-content"><img class="faq-elem-img" src="<?php bloginfo('template_url'); ?>/assets/img/content/faq/faq-img-3.png" alt="FAQ Image #3"><span class="faq-elem-number">03</span><span class="faq-elem-title">
-                 Blockchains 101</span><span class="faq-elem-text">
-                Blockchains are a type of database where
-                data is stored in blocks that are chained
-                together. As new data comes in, it is
-                entered into a block, which is then chained
-                to the previous block. The Bitcoin blockchain
-                serves as a ledger that records every Bitcoin
-                transaction in history. </span></div></a><a class="faq-elem" href="">
-                        <div class="faq-elem-content"><img class="faq-elem-img" src="<?php bloginfo('template_url'); ?>/assets/img/content/faq/faq-img-4.png" alt="FAQ Image #4"><span class="faq-elem-number">04</span><span class="faq-elem-title">
-                 Bitcoin in the Real World</span><span class="faq-elem-text">
-                Ok - you’ve found an exchange, bought your
-                Bitcoin, and stored it safely. What’s next?
-                By now you can buy just about anything with
-                Bitcoin if you try hard enough, from Amazon
-                gift cards to an old master painting. You can
-                also convert your BTC to fiat with ATMs, an
-                exchange, and more.</span></div></a></div>
             </section>
         </div>
     </main>
